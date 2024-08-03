6f8528e70d167ce0987fe8eedb503f3a64553559
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-08-03 12:34:19 -0300

    Atualizacao dos estudos.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,15 +1,30 @@
-﻿Album albumSlipknot = new Album();
-albumSlipknot.Nome = "Before Forget";
+﻿
+Banda slipknot = new Banda("Slipknot");
+
+Album albumSlipknot = new Album("Before Forget");
 
 
-Musica musica1 = new Musica();
-musica1.Nome = "Duality";
+Musica musica1 = new Musica(slipknot, "Duality")
+{
+    Duracao = 213,
+    Disponivel = true
+};
 
 
-Musica musica2 = new Musica();
-musica2.Nome = "Spirit it out";
+
+Musica musica2 = new Musica(slipknot, "Spirit it out")
+{
+    Duracao = 260,
+    Disponivel = false
+ };
+
+
 
 albumSlipknot.AdicionarMusica(musica1);
 albumSlipknot.AdicionarMusica(musica2);
+slipknot.AdicionarAlbum(albumSlipknot);
 
-
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumSlipknot.ExibirMusicasDoAlbum();
+slipknot.ExibirDiscografia();
