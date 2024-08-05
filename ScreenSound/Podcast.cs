28d3c77c02219ee071131b8053f69e6f546fdd7c
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-08-05 16:03:27 -0300

    atualização - 05082024

---
+++ ScreenSound/Podcast.cs
@@ -0,0 +1,40 @@
+﻿class Podcast
+{
+
+    private List<Episodio> episodios = new();
+
+    public string Nome { get; }
+    public string Host { get; }
+
+
+
+    public int TotaolEpsodios => episodios.Count;
+
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+        
+    }
+
+
+
+    public void AdicionarEpisodio(Episodio episodio) {
+
+     episodios.Add(episodio);
+
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast {Nome} apresenado por {Host}\n");
+        
+        foreach (Episodio episodio in episodios)
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+
+        Console.WriteLine($"\n\nEste podcast possui {TotaolEpsodios} epsodios.");
+
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -28,3 +28,22 @@ musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
 albumSlipknot.ExibirMusicasDoAlbum();
 slipknot.ExibirDiscografia();
+
+
+
+Episodio ep1 = new Episodio(1, "Tecnica de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+
+Episodio ep2 = new Episodio(2, "Tecnica de apredizado", 67);
+ep2.AdicionarConvidados("Enrique");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Camila");
+
+
+Podcast podcast = new("Podcast especoal", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
